9618ed4038f6f320a665b5736df47fd16528dba0
Luis Piñon Ferrer <luis.pinanferrer@nike.com>
2021-05-13 11:56:21 +0200

    Added kafka connector

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main():
         "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
         Constants.CURRENT_DATA_DELTA_TABLE_NAME: Constants.CURRENT_DATA,
         Constants.DELTA_SRC: Constants.DELTA_LOCATION,
+        Constants.KAFKA_SERVER: Constants.KAFKA_SERVER_NAME,
         # Constants.POSTGRESQL_DB: Constants.POSTGRESQL_DB_VALUE,
         # Constants.POSTGRESQL_USER: Constants.POSTGRESQL_USER_VALUE,
         # Constants.POSTGRESQL_PASSWORD: Constants.POSTGRESQL_PASSWORD_VALUE,
